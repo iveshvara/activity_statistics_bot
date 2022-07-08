2860b93baa4f3e4a662a7b0b14719e2baaa50bd8
iveshvara <54269229+iveshvara@users.noreply.github.com>
2022-07-08 14:36:24 +0300

    ok

--- main.py
+++ main.py
@@ -175,4 +175,114 @@ async def process_parameter(callback: CallbackQuery):
         await process_parameter_continuation(callback, id_chat, parameter_name, parameter_value)
 
 
+@dp.message_handler(content_types=['any'])
+async def message_handler(message):
+    id_chat = message.chat.id
+    title = message.chat.title
+    # title = re.sub(r'([\"])',    r'\\\1', title)
+    # title = re.escape(title)
+    # title = json.dumps(title)
+    # title = shlex.quote(title)
+
+    id_user = message.from_user.id
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    if last_name is None:
+        last_name = ''
+    username = message.from_user.username
+    if username is None:
+        username = ''
+    characters = 0
+    date_of_the_last_message = message.date  # .strftime("%d.%m.%Y %H:%M:%S")
+
+    if len(message.entities) == 1 and message.entities[0].type == 'bot_command':
+        pass
+    elif message.content_type == 'text':
+        if message.from_user.is_bot:
+            return
+        characters = len(message.text)
+    elif message.content_type == 'photo' and message.caption is not None:
+        characters = len(message.caption)
+    elif message.content_type == 'poll':
+        characters = len(message.poll.question)
+        for i in message.poll.options:
+            characters += len(i.text)
+
+    if message.content_type == 'new_chat_members':
+        for i in message.new_chat_members:
+            if i.is_bot:
+                continue
+
+            i_last_name = i.last_name
+            if i_last_name is None:
+                i_last_name = ''
+
+            i_username = i.username
+            if i_username is None:
+                i_username = ''
+
+            cursor.execute(
+                'INSERT INTO chats (id_chat, id_user, first_name, last_name, username, '
+                'messages, characters, deleted, date_of_the_last_message) '
+                f'VALUES ({id_chat}, {i.id}, "{i.first_name}", "{i_last_name}", '
+                f'"{i_username}", 0, 0, False, "{date_of_the_last_message}")')
+            connect.commit()
+    elif message.content_type == 'left_chat_member':
+        i = message.left_chat_member
+        if not i.is_bot:
+            cursor.execute(f'SELECT * FROM chats WHERE id_chat = {id_chat} AND id_user = {i.id}')
+            meaning = cursor.fetchone()
+            if meaning is not None:
+                cursor.execute(f'UPDATE chats SET deleted = True WHERE id_chat = {id_chat} AND id_user = {i.id}')
+            connect.commit()
+
+    cursor.execute(f'SELECT * FROM chats WHERE id_chat = {id_chat} AND id_user = {id_user}')
+    meaning = cursor.fetchone()
+
+    if meaning is None:
+        cursor.execute(
+            f'INSERT INTO chats (id_chat, id_user, first_name, last_name, '
+            f'username, messages, characters, deleted, date_of_the_last_message) '
+            f'VALUES ({id_chat}, {id_user}, "{first_name}", "{last_name}", '
+            f'"{username}", 1, {characters}, False, "{date_of_the_last_message}")')
+    else:
+        cursor.execute('UPDATE chats SET '
+                       'messages = messages + 1, '
+                       f'characters = characters + {characters}, '
+                       f'first_name = "{first_name}", '
+                       f'last_name = "{last_name}", '
+                       f'username = "{username}", '
+                       f'deleted = False, '
+                       f'date_of_the_last_message = "{date_of_the_last_message}" '
+                       f'WHERE id_chat = {id_chat} AND id_user = {id_user}')
+    connect.commit()
+
+    cursor.execute(f'SELECT * FROM settings WHERE id_chat = {id_chat}')
+    meaning = cursor.fetchone()
+
+    # title = "'" + re.sub("[^\\da-zA-Zа-яёА-ЯЁ _""]", "", title) + "'"
+    # title = ''.join([c for c in title if c in "[^\\da-zA-Zа-яёА-ЯЁ _""]"])
+
+    title_result = ''
+    allowed_simbols = ' _[]()"'
+    for i in title:
+        if i.isalnum():
+            title_result += i
+        elif i in allowed_simbols:
+            title_result += '\\' + i
+        else:
+            pass
+    title_result = "'" + title_result + "'"
+
+    if meaning is None:
+        cursor.execute(f'INSERT INTO settings (id_chat, title, include_admins_in_statistics, sort_by_messages, '
+                       f'do_not_output_the_number_of_messages, do_not_output_the_number_of_characters, '
+                       f'days_without_activity_is_bad, report_enabled, report_every_week, report_time) ' 
+                       f'VALUES ({id_chat}, {title_result}, False, False, False, False, 7, False, False, "00:00")')
+        connect.commit()
+    else:
+        cursor.execute(f'UPDATE settings SET title = {title_result} WHERE id_chat = {id_chat}')
+    connect.commit()
+
+
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
